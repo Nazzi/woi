67bbf256134dc61d461d80d9ebf7de89f3b6d544
Nazzi <maxgraman00@gmail.com>
2015-08-28 22:40:55 -0400

    Added .psd for easy graphics + Adamantine ore.

--- src/main/java/com/nazzimods/woi/init/ModBlocks.java
+++ src/main/java/com/nazzimods/woi/init/ModBlocks.java
@@ -1,5 +1,6 @@
 package com.nazzimods.woi.init;
 
+import com.nazzimods.woi.block.BlockAdamantineOre;
 import com.nazzimods.woi.block.BlockLuskiumOre;
 import com.nazzimods.woi.block.BlockWOI;
 import com.nazzimods.woi.reference.Names;
@@ -12,9 +13,11 @@ import net.minecraft.item.Item;
 public class ModBlocks {
 
 	public static final BlockWOI oreLuskium = new BlockLuskiumOre(null);
+	public static final BlockWOI oreAdamantine = new BlockAdamantineOre(null);
 
 	public static void init() {
 		GameRegistry.registerBlock(oreLuskium, Names.Blocks.LUSKIUM_ORE);
+		GameRegistry.registerBlock(oreAdamantine, Names.Blocks.ADAMANTINE_ORE);
 
 	}
 
