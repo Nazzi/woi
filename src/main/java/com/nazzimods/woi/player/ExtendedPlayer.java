eeaadc1e352104b533268ec0b86462c97c1fdff6
Nazzi <maxgraman00@gmail.com>
2015-09-11 19:05:16 -0400

    Added some WIP stuff

---
+++ src/main/java/com/nazzimods/woi/player/ExtendedPlayer.java
@@ -0,0 +1,59 @@
+package com.nazzimods.woi.player;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+import net.minecraftforge.common.IExtendedEntityProperties;
+
+public class ExtendedPlayer implements IExtendedEntityProperties {
+
+	public final static String EXT_PROP_NAME = "ExtendedPlayer";
+
+	private final EntityPlayer player;
+
+	private int currentMana, maxMana;
+
+	public ExtendedPlayer(EntityPlayer player) {
+		this.player = player;
+		this.currentMana = this.maxMana = 50;
+	}
+
+	public static final void register(EntityPlayer player) {
+		player.registerExtendedProperties(ExtendedPlayer.EXT_PROP_NAME, new ExtendedPlayer(player));
+	}
+
+	public static final ExtendedPlayer get(EntityPlayer player) {
+		return (ExtendedPlayer) player.getExtendedProperties(EXT_PROP_NAME);
+	}
+
+	@Override
+	public void saveNBTData(NBTTagCompound compound) {
+
+		NBTTagCompound properties = new NBTTagCompound();
+
+		properties.setInteger("CurrentMana", this.currentMana);
+		properties.setInteger("MaxMana", this.maxMana);
+
+		compound.setTag(EXT_PROP_NAME, properties);
+	}
+
+	@Override
+	public void loadNBTData(NBTTagCompound compound) {
+		
+		NBTTagCompound properties = (NBTTagCompound) compound.getTag(EXT_PROP_NAME);
+
+		this.currentMana = properties.getInteger("CurrentMana");
+		this.maxMana = properties.getInteger("MaxMana");
+	
+		System.out.println("[WOI] Mana from NBT: " + this.currentMana + "/" + this.maxMana);
+
+	}
+
+	@Override
+	public void init(Entity entity, World world) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
